911d255f7a5bd4da2384a06dd5262ab038e6cf39
Mehrshad Lotfi <lotfimehrshad@gmail.com>
2024-09-18 00:25:27 +0200

    Initial commit. Add PR checks

---
+++ pr-checks/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const token = core.getInput('github-token');
+    const octokit = github.getOctokit(token);
+    const context = github.context;
+
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+    }
+
+    const prBody = context.payload.pull_request.body;
+    const regex = /notion\.so\/OPT-\d+/;
+
+    if (!regex.test(prBody)) {
+      core.setFailed('The PR body does not include a link \
+        like notion.so/OPT-xxx.');
+    } else {
+      core.info('The PR body includes a valid Notion link.');
+    } 
+  } catch (error) {
+      core.setFailed(error.message);
+  }
+}
+
+run();
